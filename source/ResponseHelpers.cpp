9e324131d724243d3c18fa72bcd7b08ec0f69549
mariia paraskun <mariiaparaskun@gmail.com>
2023-12-29 15:31:36 +0100

    over42 added htmls

--- source/ResponseHelpers.cpp
+++ source/ResponseHelpers.cpp
@@ -15,7 +15,7 @@ Response *Webserv::create_http_response(void)
         http_response->path = "/403.html";
     } else if (http_response->status_code == 404) {
         http_response->status_message = "Not Found";
-        http_response->path = "./404.html";
+        http_response->path = "./over42/404.html";
     } else if (http_response->status_code == 201) {
         http_response->body += "<html><body>";
         http_response->body += "Your data received by us";
@@ -41,6 +41,7 @@ Response *Webserv::create_http_response(void)
     }
     // http_request.path = "." + http_request.path;
     //Check if it is a file (static website), if not it's a cgi script
+    //http_response->headers["Access-Control-Allow-Origin"] = "*";
     if (http_response->status_code == 200 || http_response->status_code == 404 || http_response->status_code == 403) {
         if (access(http_response->path.c_str(), F_OK) != 0)
             http_response->body = http_response->path;
